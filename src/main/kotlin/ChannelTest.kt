cf7dee936aff3d304e110184c17d7a426cb9a94d
Mark <mazhenjin@sina.com>
2023-03-02 08:43:14 +0800

    kotlin channel

---
+++ src/main/kotlin/ChannelTest.kt
@@ -0,0 +1,37 @@
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+
+class ChannelTest {
+}
+
+
+
+fun main() = runBlocking {
+    val numbers = produceNumbers() // produces integers from 1 and on
+    val squares = square(numbers) // squares integers
+    repeat(5) {
+        println(squares.receive()) // print first five
+    }
+    println("Done!") // we are done
+    coroutineContext.cancelChildren() // cancel children coroutines
+
+    val c = Channel<Int>(Channel.UNLIMITED)
+
+}
+
+fun CoroutineScope.produceNumbers() = produce<Int> {
+    var x = 1
+    while (true) send(x++) // infinite stream of integers starting from 1
+}
+
+fun CoroutineScope.square(numbers: ReceiveChannel<Int>): ReceiveChannel<Int> = produce {
+    for (x in numbers) send(x * x)
+}
+
+fun CoroutineScope.filter(numbers: ReceiveChannel<Int>, prime: Int) = produce<Int> {
+    for (x in numbers) if (x % prime != 0) send(x)
+}
